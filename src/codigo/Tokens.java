20a9bd20b8b50e8949d81253c3533cc41c1536ec
AGCCodeX <alvarogctec@gmail.com>
2023-05-10 19:47:00 -0600

    Primer Commit

---
+++ src/codigo/Tokens.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package codigo;
+
+
+public enum Tokens {
+    Puntuacion,
+    Agrupacion,
+    OperadorArit,
+    OperadorAsig,
+    SaltoLinea,
+    Reservada,
+    id,
+    num,
+    Fin,
+    ERROR
+}
